b5b4f0453ae26310927b9ed720b37ae4c2d52c23
yusing <yusing@6uo.me>
2025-03-29 16:10:33 +0800

    chore: remove unused AgentRegistrationPort environment variable from env.go

--- agent/pkg/env/env.go
+++ agent/pkg/env/env.go
@@ -17,7 +17,6 @@ func DefaultAgentName() string {
 var (
 	AgentName                = common.GetEnvString("AGENT_NAME", DefaultAgentName())
 	AgentPort                = common.GetEnvInt("AGENT_PORT", 8890)
-	AgentRegistrationPort    = common.GetEnvInt("AGENT_REGISTRATION_PORT", 8891)
 	AgentSkipClientCertCheck = common.GetEnvBool("AGENT_SKIP_CLIENT_CERT_CHECK", false)
 
 	AgentCACert  = common.GetEnvString("AGENT_CA_CERT", "")
