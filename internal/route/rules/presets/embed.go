bef375ef6c2f0a18d9ccbc8f7c861f6ff9eb781a
yusing <yusing.wys@gmail.com>
2025-10-14 16:20:32 +0800

    fix: load embed rules lazily and do not panic

--- internal/route/rules/presets/embed.go
+++ internal/route/rules/presets/embed.go
@@ -2,8 +2,8 @@ package rulepresets
 
 import (
 	"embed"
-	"fmt"
 	"reflect"
+	"sync"
 
 	"github.com/rs/zerolog/log"
 	"github.com/yusing/godoxy/internal/route/rules"
@@ -15,26 +15,32 @@ var fs embed.FS
 
 var rulePresets = make(map[string]rules.Rules)
 
+var once sync.Once
+
 func GetRulePreset(name string) (rules.Rules, bool) {
+	once.Do(initPresets)
 	rules, ok := rulePresets[name]
 	return rules, ok
 }
 
-// init all rule presets
-func init() {
+// init all rule presetsl lazily
+func initPresets() {
 	files, err := fs.ReadDir(".")
 	if err != nil {
-		panic(err)
+		log.Error().Err(err).Msg("failed to read rule presets")
+		return
 	}
 	for _, file := range files {
 		var rules rules.Rules
 		content, err := fs.ReadFile(file.Name())
 		if err != nil {
-			panic(fmt.Errorf("failed to read rule preset %s: %w", file.Name(), err))
+			log.Error().Str("name", file.Name()).Err(err).Msg("failed to read rule preset")
+			continue
 		}
 		_, err = serialization.ConvertString(string(content), reflect.ValueOf(&rules))
 		if err != nil {
-			panic(fmt.Errorf("failed to unmarshal rule preset %s: %w", file.Name(), err))
+			log.Error().Str("name", file.Name()).Err(err).Msg("failed to unmarshal rule preset")
+			continue
 		}
 		rulePresets[file.Name()] = rules
 		log.Debug().Str("name", file.Name()).Msg("loaded rule preset")
