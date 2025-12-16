ba8d23fada294e5e4524ec402ceb606b462c263d
yusing <yusing.wys@gmail.com>
2025-12-16 15:04:21 +0800

    fix(config): nil panic introduced in ff934a4bb2911f5fa3c23d8fe6fea252d881fdc3; remove duplicated log

--- internal/watcher/health/monitor/monitor.go
+++ internal/watcher/health/monitor/monitor.go
@@ -74,15 +74,7 @@ func NewMonitor(r types.Route) types.HealthMonCheck {
 }
 
 func newMonitor(u *url.URL, cfg types.HealthCheckConfig, healthCheckFunc HealthCheckFunc) *monitor {
-	state := config.WorkingState.Load()
-	if state == nil {
-		state = config.ActiveState.Load()
-	}
-	if state != nil {
-		cfg.ApplyDefaults(state.Value().Defaults.HealthCheck)
-	} else {
-		cfg.ApplyDefaults(types.HealthCheckConfig{}) // use config defaults
-	}
+	cfg.ApplyDefaults(config.WorkingState.Load().Value().Defaults.HealthCheck)
 	mon := &monitor{
 		config:      cfg,
 		checkHealth: healthCheckFunc,
