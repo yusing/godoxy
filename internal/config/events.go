yusing/godoxy

commit ff934a4bb2911f5fa3c23d8fe6fea252d881fdc3

fix(config): fix default values not applied

internal/config/events.go
@@ -52,11 +52,16 @@ func logNotifyWarn(action string, err error) {
 	})
 }
 
+var nilState *state
+
 func Load() error {
 	if HasState() {
 		panic(errors.New("config already loaded"))
 	}
 	state := NewState()
+	config.WorkingState.Store(state)
+	defer config.WorkingState.Store(nilState)
+
 	cfgWatcher = watcher.NewConfigFileWatcher(common.ConfigFileName)
 
 	initErr := state.InitFromFile(common.ConfigPath)
@@ -82,6 +87,9 @@ func Reload() gperr.Error {
 	defer reloadMu.Unlock()
 
 	newState := NewState()
+	config.WorkingState.Store(newState)
+	defer config.WorkingState.Store(nilState)
+
 	err := newState.InitFromFile(common.ConfigPath)
 	if err != nil {
 		newState.Task().FinishAndWait(err)

internal/config/types/state.go
@@ -33,7 +33,10 @@ type State interface {
 	FlushTmpLog()
 }
 
-// could be nil
+// could be nil before first call on Load
 var ActiveState synk.Value[State]
+
+// nil-safe while loading config, nil otherwise
+var WorkingState synk.Value[State]
 
 var ErrConfigChanged = errors.New("config changed")

internal/watcher/health/monitor/monitor.go
@@ -74,7 +74,15 @@ func NewMonitor(r types.Route) types.HealthMonCheck {
 }
 
 func newMonitor(u *url.URL, cfg types.HealthCheckConfig, healthCheckFunc HealthCheckFunc) *monitor {
-	cfg.ApplyDefaults(config.DefaultConfig().Defaults.HealthCheck)
+	state := config.WorkingState.Load()
+	if state == nil {
+		state = config.ActiveState.Load()
+	}
+	if state != nil {
+		cfg.ApplyDefaults(state.Value().Defaults.HealthCheck)
+	} else {
+		cfg.ApplyDefaults(types.HealthCheckConfig{}) // use config defaults
+	}
 	mon := &monitor{
 		config:      cfg,
 		checkHealth: healthCheckFunc,

internal/route/route.go
@@ -774,7 +774,7 @@ func (r *Route) Finalize() {
 	}
 
 	r.Port.Listening, r.Port.Proxy = lp, pp
-	r.HealthCheck.ApplyDefaults(config.ActiveConfig.Load().Defaults.HealthCheck)
+	r.HealthCheck.ApplyDefaults(config.WorkingState.Load().Value().Defaults.HealthCheck)
 }
 
 func (r *Route) FinalizeHomepageConfig() {

internal/types/healthcheck_config.go
@@ -36,6 +36,9 @@ func (hc *HealthCheckConfig) ApplyDefaults(defaults HealthCheckConfig) {
 		}
 	}
 	if hc.Retries == 0 {
-		hc.Retries = int64(HealthCheckDownNotifyDelayDefault / hc.Interval)
+		hc.Retries = defaults.Retries
+		if hc.Retries == 0 {
+			hc.Retries = max(1, int64(HealthCheckDownNotifyDelayDefault/hc.Interval))
+		}
 	}
 }

commit 6f9f9951006917735dc4dfa4a3c9ba151185b31d

fix(config): nil panic introduced in ff934a4bb2911f5fa3c23d8fe6fea252d881fdc3; remove duplicated log

internal/config/events.go
@@ -52,15 +52,12 @@ func logNotifyWarn(action string, err error) {
 	})
 }
 
-var nilState *state
-
 func Load() error {
 	if HasState() {
 		panic(errors.New("config already loaded"))
 	}
 	state := NewState()
 	config.WorkingState.Store(state)
-	defer config.WorkingState.Store(nilState)
 
 	cfgWatcher = watcher.NewConfigFileWatcher(common.ConfigFileName)
 
@@ -88,11 +85,11 @@ func Reload() gperr.Error {
 
 	newState := NewState()
 	config.WorkingState.Store(newState)
-	defer config.WorkingState.Store(nilState)
 
 	err := newState.InitFromFile(common.ConfigPath)
 	if err != nil {
 		newState.Task().FinishAndWait(err)
+		config.WorkingState.Store(GetState())
 		logNotifyError("reload", err)
 		return gperr.New(ansi.Warning("using last config")).With(err)
 	}
@@ -106,7 +103,6 @@ func Reload() gperr.Error {
 	SetState(newState)
 
 	if err := newState.StartProviders(); err != nil {
-		gperr.LogWarn("start providers error", err)
 		logNotifyError("start providers", err)
 		return nil // continue
 	}
@@ -121,7 +117,7 @@ func WatchChanges() {
 		configEventFlushInterval,
 		OnConfigChange,
 		func(err gperr.Error) {
-			gperr.LogError("config reload error", err)
+			logNotifyError("config reload", err)
 		},
 	)
 	eventQueue.Start(cfgWatcher.Events(t.Context()))

internal/config/types/state.go
@@ -36,7 +36,7 @@ type State interface {
 // could be nil before first call on Load
 var ActiveState synk.Value[State]
 
-// nil-safe while loading config, nil otherwise
+// working state while loading config, same as ActiveState after successful load
 var WorkingState synk.Value[State]
 
 var ErrConfigChanged = errors.New("config changed")

internal/watcher/health/monitor/monitor.go
@@ -74,15 +74,7 @@ func NewMonitor(r types.Route) types.HealthMonCheck {
 }
 
 func newMonitor(u *url.URL, cfg types.HealthCheckConfig, healthCheckFunc HealthCheckFunc) *monitor {
-	state := config.WorkingState.Load()
-	if state == nil {
-		state = config.ActiveState.Load()
-	}
-	if state != nil {
-		cfg.ApplyDefaults(state.Value().Defaults.HealthCheck)
-	} else {
-		cfg.ApplyDefaults(types.HealthCheckConfig{}) // use config defaults
-	}
+	cfg.ApplyDefaults(config.WorkingState.Load().Value().Defaults.HealthCheck)
 	mon := &monitor{
 		config:      cfg,
 		checkHealth: healthCheckFunc,