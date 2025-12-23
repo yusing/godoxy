644db930c0c681cddef3e7db6f5fe1baab074b0a
yusing <yusing.wys@gmail.com>
2025-12-23 10:41:03 +0800

    refactor(types): remove unnecessary TLSConfig.BuildTLSConfig() method

--- internal/types/tls.go
+++ internal/types/tls.go
@@ -1,38 +1,7 @@
 package types
 
-import (
-	"crypto/tls"
-	"crypto/x509"
-	"errors"
-	"os"
-)
-
 type TLSConfig struct {
 	CAFile   string `json:"ca_file,omitempty"`
 	CertFile string `json:"cert_file,omitempty"`
 	KeyFile  string `json:"key_file,omitempty"`
-}
-
-func (cfg *TLSConfig) BuildTLSConfig() (*tls.Config, error) {
-	tlsConfig := &tls.Config{}
-	if cfg.CAFile != "" {
-		ca, err := os.ReadFile(cfg.CAFile)
-		if err != nil {
-			return nil, err
-		}
-		tlsConfig.RootCAs = x509.NewCertPool()
-		if ok := tlsConfig.RootCAs.AppendCertsFromPEM(ca); !ok {
-			return nil, errors.New("failed to append certificates from PEM")
-		}
-	}
-
-	if cfg.CertFile != "" && cfg.KeyFile != "" {
-		cert, err := tls.LoadX509KeyPair(cfg.CertFile, cfg.KeyFile)
-		if err != nil {
-			return nil, err
-		}
-		tlsConfig.Certificates = []tls.Certificate{cert}
-	}
-
-	return tlsConfig, nil
 }
