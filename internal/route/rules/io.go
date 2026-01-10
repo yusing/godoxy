966f0ab9c35ccf8992ef0563b7b18c285dcbd595
yusing <yusing.wys@gmail.com>
2026-01-10 18:40:06 +0800

    fix(rules): simplify and correct tests

--- internal/route/rules/io.go
+++ internal/route/rules/io.go
@@ -1,9 +1,14 @@
 package rules
 
 import (
+	"bytes"
+	"fmt"
 	"io"
+	"math/rand"
 	"os"
+	"sync"
 
+	"github.com/yusing/godoxy/internal/common"
 	"github.com/yusing/godoxy/internal/logging/accesslog"
 	gperr "github.com/yusing/goutils/errs"
 )
@@ -21,6 +26,11 @@ var (
 	stderr io.WriteCloser = noopWriteCloser{os.Stderr}
 )
 
+var (
+	testFiles     = make(map[string]*bytes.Buffer)
+	testFilesLock sync.Mutex
+)
+
 func openFile(path string) (io.WriteCloser, gperr.Error) {
 	switch path {
 	case "/dev/stdout":
@@ -28,9 +38,36 @@ func openFile(path string) (io.WriteCloser, gperr.Error) {
 	case "/dev/stderr":
 		return stderr, nil
 	}
+
+	if common.IsTest {
+		testFilesLock.Lock()
+		defer testFilesLock.Unlock()
+		if buf, ok := testFiles[path]; ok {
+			return noopWriteCloser{buf}, nil
+		}
+		buf := bytes.NewBuffer(nil)
+		testFiles[path] = buf
+		return noopWriteCloser{buf}, nil
+	}
+
 	f, err := accesslog.NewFileIO(path)
 	if err != nil {
 		return nil, ErrInvalidArguments.With(err)
 	}
 	return f, nil
+}
+
+func TestRandomFileName() string {
+	return fmt.Sprintf("test-file-%d.txt", rand.Intn(1000000))
+}
+
+func TestFileContent(path string) []byte {
+	testFilesLock.Lock()
+	defer testFilesLock.Unlock()
+
+	buf, ok := testFiles[path]
+	if !ok {
+		return nil
+	}
+	return buf.Bytes()
 }
