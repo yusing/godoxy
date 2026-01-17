b1953d86c2bd0f426e20df8105b13e3be7f79c94
yusing <yusing.wys@gmail.com>
2026-01-17 16:07:40 +0800

    fix(idlewatcher): remove duplicated w.readyNotifyCh notification

--- internal/idlewatcher/watcher.go
+++ internal/idlewatcher/watcher.go
@@ -612,10 +612,6 @@ func (w *Watcher) watchUntilDestroy() (returnCause error) {
 				if ready {
 					// Container is now ready, notify waiting handlers
 					w.healthTicker.Stop()
-					select {
-					case w.readyNotifyCh <- struct{}{}:
-					default: // channel full, notification already pending
-					}
 					w.resetIdleTimer()
 				}
 				// If not ready yet, keep checking on next tick
