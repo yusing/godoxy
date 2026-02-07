f73ed3811569196608902d16c1e5d79180f79494
yusing <yusing.wys@gmail.com>
2026-02-07 20:24:38 +0800

    test: improve tests

--- internal/route/test_route.go
+++ internal/route/test_route.go
@@ -1,12 +1,22 @@
 package route
 
 import (
+	"testing"
+
+	"github.com/yusing/godoxy/internal/entrypoint"
+	epctx "github.com/yusing/godoxy/internal/entrypoint/types"
 	"github.com/yusing/godoxy/internal/types"
 	"github.com/yusing/goutils/task"
 )
 
-func NewTestRoute[T interface{ Helper() }](t T, task task.Parent, base *Route) (types.Route, error) {
+func NewStartedTestRoute(t testing.TB, base *Route) (types.Route, error) {
 	t.Helper()
+
+	task := task.GetTestTask(t)
+	if ep := epctx.FromCtx(task.Context()); ep == nil {
+		ep = entrypoint.NewEntrypoint(task, nil)
+		epctx.SetCtx(task, ep)
+	}
 
 	err := base.Validate()
 	if err != nil {
