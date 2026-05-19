20bdc8e958ee847b13699513e10036284775e6cc
yusing <yusing.wys@gmail.com>
2026-05-19 12:27:05 +0800

    chore: remove unused constants

--- internal/common/constants.go
+++ internal/common/constants.go
@@ -3,25 +3,18 @@ package common
 // file, folder structure
 
 const (
-	DotEnvPath        = ".env"
-	DotEnvExamplePath = ".env.example"
-
-	ConfigBasePath        = "config"
-	ConfigFileName        = "config.yml"
-	ConfigExampleFileName = "config.example.yml"
-	ConfigPath            = ConfigBasePath + "/" + ConfigFileName
+	ConfigBasePath = "config"
+	ConfigFileName = "config.yml"
+	ConfigPath     = ConfigBasePath + "/" + ConfigFileName
 
 	DataDir           = "data"
 	IconListCachePath = DataDir + "/.icon_list_cache.json"
 
 	NamespaceHomepageOverrides = ".homepage"
-	NamespaceIconCache         = ".icon_cache"
 
 	MiddlewareComposeBasePath = ConfigBasePath + "/middlewares"
 
-	ComposeFileName        = "compose.yml"
-	ComposeExampleFileName = "compose.example.yml"
-	ErrorPagesBasePath     = "error_pages"
+	ErrorPagesBasePath = "error_pages"
 )
 
 var RequiredDirectories = []string{
